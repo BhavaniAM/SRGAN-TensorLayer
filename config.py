de8901d27df4016ed99bdfacc2fe073694138bcf
Bhavani AM <58398927+BhavaniAM@users.noreply.github.com>
2023-02-02 14:55:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,24 +8,26 @@ config.TRAIN.lr_init = 1e-4
 config.TRAIN.beta1 = 0.9
 
 ## initialize G
-config.TRAIN.n_epoch_init = 100
+config.TRAIN.n_epoch_init = 50 # 100
     # config.TRAIN.lr_decay_init = 0.1
     # config.TRAIN.decay_every_init = int(config.TRAIN.n_epoch_init / 2)
 
 ## adversarial learning (SRGAN)
-config.TRAIN.n_epoch = 2000
+config.TRAIN.n_epoch = 10 # 2000
 config.TRAIN.lr_decay = 0.1
 config.TRAIN.decay_every = int(config.TRAIN.n_epoch / 2)
 
 ## train set location
 # config.TRAIN.hr_img_path = 'DIV2K/DIV2K_train_HR/'
-config.TRAIN.hr_img_path = 'images/'
-config.TRAIN.lr_img_path = 'DIV2K/DIV2K_train_LR_bicubic/X4/'
+config.TRAIN.hr_img_path = 'ISRO/Train_HR/'
+# config.TRAIN.lr_img_path = 'DIV2K/DIV2K_train_LR_bicubic/X4/'
+config.TRAIN.lr_img_path = 'ISRO/Train_LR/'
+
 
 config.VALID = edict()
 ## test set location
-config.VALID.hr_img_path = 'DIV2K/DIV2K_valid_HR/'
-config.VALID.lr_img_path = 'DIV2K/DIV2K_valid_LR_bicubic/X4/'
+config.VALID.hr_img_path = 'ISRO/ISRO_valid_HR/'
+config.VALID.lr_img_path = 'ISRO/ISRO_valid_LR/'
 
 def log_config(filename, cfg):
     with open(filename, 'w') as f:
